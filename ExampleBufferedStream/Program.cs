5c5d32d3795f393bd737293c142f6b51658b5737
DouglasRivas94 <rivasdouglas284@gmail.com>
2025-03-13 09:23:11 -0600

    13/03/2025

--- ExampleBufferedStream/Program.cs
+++ ExampleBufferedStream/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using ExampleBufferedStream;
+
+string archivoProducto = "Productos.txt";
+
+IRepositorioProducto RepositorioProducto = new RepositorioBufferedProducto(archivoProducto);
+
+RepositorioProducto.add(new Producto
+{
+    Id = 1,
+    Name = "Test",
+    Precio = 10.5m
+});RepositorioProducto.add(new Producto
+{
+    Id = 1,
+    Name = "Test2",
+    Precio = 12.5m
+});
+
+Console.WriteLine("Todos los productos");
+foreach(var producto in RepositorioProducto.GetAll())
+{
+    Console.WriteLine($"ID: {producto.Id}, Nombre: {producto.Name}, Precio: {producto.Precio}");
+}
